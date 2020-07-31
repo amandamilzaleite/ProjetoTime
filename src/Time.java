12928cc57bbe9726b7abe014f94e044d3ddac316
amandamilzaleite <amandamilzaleite@gmail.com>
2020-07-31 16:53:36 -0300

    primeiro commit

---
+++ src/Time.java
@@ -0,0 +1,17 @@
+
+public class Time {
+	private int hora;
+	private int minuto;
+	private int segundo;
+	
+	void setTime(int hora, int minuto, int segundo){
+		this.hora = hora;
+		this.minuto = minuto;
+		this.segundo = segundo;
+	}
+	
+	String exibirHoraUniversal() {
+		
+	}
+
+}
